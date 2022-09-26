692a8d97166d208797df8a0eeaf357da57b4e23e
Monique Rio <mrio@umich.edu>
2022-09-26 13:51:42 -0400

    BREAKING CHANGE: changed name of the module
    BEGIN_COMMIT_OVERRIDE
    fix!: changed name of module
    END_COMMIT_OVERRIDE

--- lib/release-please.rb
+++ lib/release-please.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "demo/version"
+require_relative "release-please/version"
 
-module Demo
+module ReleasePlease
   class Error < StandardError; end
   # Your code goes here...
 end
